1e80b14486cfb00023f215f7483add6d1229dd26
Rashwan <rashwanmohmed2@gmail.com>
2022-07-27 18:30:02 +0200

    deploy

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import file from './data.json'
+import img from './jeremy.png'
 
 function App() {
+  const [when, setWhen] = useState('daily')
+  const [value, setValue] = useState()
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <main>
+      <section className='first'>
+        <div className='img-box'>
+          <img src={img} alt='' />
+          <div>
+            <h6>report for</h6>
+            <p>
+              jermy <br /> Robson
+            </p>
+          </div>
+        </div>
+        <div className='assd'>
+          <button
+            className={value === 0 && 'look'}
+            onClick={() => {
+              setWhen('daily')
+              setValue(0)
+            }}
+          >
+            daily
+          </button>
+          <button
+            className={value === 1 && 'look'}
+            onClick={() => {
+              setWhen('weekly')
+              setValue(1)
+            }}
+          >
+            weekly
+          </button>
+          <button
+            className={value === 2 && 'look'}
+            onClick={() => {
+              setWhen('monthly')
+              setValue(2)
+            }}
+          >
+            monthly
+          </button>
+        </div>
+      </section>
+      {file.map((per, index) => {
+        return (
+          <article key={index} className={per.title}>
+            <div className='item'>
+              <h5>
+                {per.title} <span>...</span>
+              </h5>
+              <h3>{per.timeframes[when].current}hrs</h3>
+              <p>last week - {per.timeframes[when].previous} hrs</p>
+            </div>
+          </article>
+        )
+      })}
+    </main>
+  )
 }
 
-export default App;
+export default App
